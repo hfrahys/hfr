e832416be3ed81d51aef80ea01137be8add6593e
hfrahys <148315412+hfrahys@users.noreply.github.com>
2023-10-19 19:17:36 +0800

    Add files via upload

---
+++ 10.19.c/10.19.c/10.19.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int a = 10, b = 30, c = 5;
+    int r1 = 0, r2 = 0, r3 = 0;
+    r1 = a + b * c;
+    r2 = (a + b * 2) % 10 > c;
+    r3!= b / a;
+    printf("%d", r3!=b/a>2);
+    return 0;
+}
